5be7b289531e9b006d2152e73acc213c0c5d2320
felixstiven <felixstiven12@gmail.com>
2024-06-27 18:31:07 -0500

    actualizando codigo html y css

--- app.js
+++ app.js
@@ -1,13 +1,63 @@
-//MENU LATERAL
-var mneu_visible = false;
+var menu_visible = false;
 let menu = document.getElementById("nav");
-function mostar0cultarmenu(){
-    if(mneu_visible==false){//si esta oculto
-        menu.style.display = "block";
-        mneu_visible = true;
-    }
-    else{
+function mostrarOcultarmenu(){
+    if(menu_visible==false){//si esta oculto
+        menu.style.display= "block";
+    menu_visible= true;
+}
+else{
+    menu.style.display= "none";
+    menu_visible = false;
+}
+}
+
+// oculto el menu una vez que selecciono una opcion
+
+let links = document.querySelectorAll("nav a");
+for(var x =0; x <links.length;x++){
+    links[x].onclick = function (){
         menu.style.display = "none";
-        mneu_visible = false;
+        menu_visible = false
     }
+}
+
+// creo las barritas de una barra particular identificada por su id
+
+function crearBarra(id_barra){
+    for(i=0;i<=16;i++){
+        let div = document.createElement("div");
+        div.className = "e"
+        id_barra.appendChild(div);
+    }
+}
+
+// selecciono todas las barras generales para luego manipiularlas
+
+let html = document.getElementById("html");
+crearBarra(html);
+let javascript = document.getElementById("javascript");
+crearBarra(javascript);
+let phyton = document.getElementById("phyton");
+crearBarra(phyton);
+let java = document.getElementById("java");
+crearBarra(java);
+let php = document.getElementById("php");
+crearBarra(php);
+let github = document.getElementById("github");
+crearBarra(github);
+
+
+// Ahora voy a guardar la cantidad de barritas que se van a ir pintando por cada barra.
+// para eso utilizo un arreglo, cada posicion pertenece a un elemento
+// comienza en 1- porque no tiene ninguna pintada al iniciarse
+
+let contadores = [-1,-1,-1,-1,-1,-1,];
+// esta variable la voy a utilizar de bandera para saber si ya ejecuto la animacion.
+let entro = false;
+
+// funcion que aplica las animaciones de la habiliadades 
+
+function efectoHabilidades(){
+    var habilidades = document.getElementById("habilidades");
+    var distancia_skills = 
 }
