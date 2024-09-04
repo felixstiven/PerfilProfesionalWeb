5841224b1fa65f279aeca9f1a0cd7641ec50274e
felixstiven <felixstiven12@gmail.com>
2024-09-04 08:12:17 -0500

    agregando funcionalidad con javascript

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var menu_visible = false;
 let menu = document.getElementById("nav");
 function mostrarOcultarmenu(){
-    if(menu_visible==false){//si esta oculto
+    if(menu_visible == false){//si esta oculto
         menu.style.display= "block";
     menu_visible= true;
 }
@@ -14,50 +14,83 @@ else{
 // oculto el menu una vez que selecciono una opcion
 
 let links = document.querySelectorAll("nav a");
-for(var x =0; x <links.length;x++){
+for(var x = 0; x < links.length; x++){
     links[x].onclick = function (){
         menu.style.display = "none";
         menu_visible = false
     }
 }
 
-// creo las barritas de una barra particular identificada por su id
+// Funcionalidad barras de habilidades
 
-function crearBarra(id_barra){
-    for(i=0;i<=16;i++){
-        let div = document.createElement("div");
-        div.className = "e"
-        id_barra.appendChild(div);
-    }
+const progressHtml= document.getElementById("htmlCss");
+const progressJavaScript = document.getElementById("javascript");
+const progressPython = document.getElementById("python");
+const progressJava = document.getElementById("java");
+const progressReact = document.getElementById("react");
+const progressGithub = document.getElementById("github");
+
+const controller = new AbortController();
+
+
+// const txtBarras = {
+//     html: "80%",
+//     javascript: "30%" 
+
+
+
+
+progressHtml.addEventListener("click", unaFuctionHtml)
+
+function unaFuctionHtml (e){
+    const value = 88;
+    const barraTemplate = "88%"
+    const barraTxtElement = document.createElement("p")
+    const txtHtml = document.getElementById("txthtml");
+    barraTxtElement.innerHTML = barraTemplate;
+    progressHtml.appendChild(barraTxtElement);
+    progressHtml.style.setProperty("--progress", `${value}% `);
+    barraTxtElement.style.fontSize= "25px";
+    txtHtml.style.color=("yellow");
+
+    progressHtml.removeEventListener("click", unaFuctionHtml);
 }
 
-// selecciono todas las barras generales para luego manipiularlas
+progressJavaScript.addEventListener("click", unaFuctionJavascript)
 
-let html = document.getElementById("html");
-crearBarra(html);
-let javascript = document.getElementById("javascript");
-crearBarra(javascript);
-let phyton = document.getElementById("phyton");
-crearBarra(phyton);
-let java = document.getElementById("java");
-crearBarra(java);
-let php = document.getElementById("php");
-crearBarra(php);
-let github = document.getElementById("github");
-crearBarra(github);
+function unaFuctionJavascript (e){
+    const value = 30 ;
+    const barraTemplate = "30%";
+    const barraTxtElement = document.createElement("p");
+    const txtJavascript = document.getElementById("javascript");
+    barraTxtElement.innerHTML = barraTemplate;
+    progressJavaScript.appendChild(barraTxtElement);
+    progressJavaScript.style.setProperty("--progress", `${value}%`);
+    barraTxtElement.style.fontSize= "25px";
+    txtJavascript.style.color=("yellow");
+
+    progressJavaScript.removeEventListener("click", unaFuctionJavascript)
 
 
-// Ahora voy a guardar la cantidad de barritas que se van a ir pintando por cada barra.
-// para eso utilizo un arreglo, cada posicion pertenece a un elemento
-// comienza en 1- porque no tiene ninguna pintada al iniciarse
-
-let contadores = [-1,-1,-1,-1,-1,-1,];
-// esta variable la voy a utilizar de bandera para saber si ya ejecuto la animacion.
-let entro = false;
-
-// funcion que aplica las animaciones de la habiliadades 
-
-function efectoHabilidades(){
-    var habilidades = document.getElementById("habilidades");
-    var distancia_skills = 
 }
+
+
+
+
+
+progressPython.addEventListener("click",() =>{
+    const value = 10 ;
+    progressPython.style.setProperty("--progress", `${value}%`)
+})
+progressJava.addEventListener("click",() =>{
+    const value = 10;
+    progressJava.style.setProperty("--progress", `${value}%`)
+})
+progressReact.addEventListener("click",() =>{
+    const value = 20;
+    progressReact.style.setProperty("--progress", `${value}%`)
+})
+progressGithub.addEventListener("click",() =>{
+    const value = 50;
+    progressGithub.style.setProperty("--progress", `${value}%`)
+})
