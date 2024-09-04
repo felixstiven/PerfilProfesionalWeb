08b5b3d4b6932475e287736da3cfe0c4680bd11b
felixstiven <felixstiven12@gmail.com>
2024-09-04 08:55:54 -0500

    agregando fucnionalidad con javascript

--- app.js
+++ app.js
@@ -22,75 +22,118 @@ for(var x = 0; x < links.length; x++){
 }
 
 // Funcionalidad barras de habilidades
-
 const progressHtml= document.getElementById("htmlCss");
-const progressJavaScript = document.getElementById("javascript");
-const progressPython = document.getElementById("python");
-const progressJava = document.getElementById("java");
-const progressReact = document.getElementById("react");
-const progressGithub = document.getElementById("github");
-
+const progressJavaScript = document.getElementById("javascript-1");
+const progressPython = document.getElementById("python-1");
+const progressSql = document.getElementById("sql-1");
+const progressReact = document.getElementById("react-1");
+const progressGithub = document.getElementById("github-1");
 const controller = new AbortController();
 
-
-// const txtBarras = {
-//     html: "80%",
-//     javascript: "30%" 
-
-
-
-
+// Evento barra html
 progressHtml.addEventListener("click", unaFuctionHtml)
-
 function unaFuctionHtml (e){
     const value = 88;
     const barraTemplate = "88%"
     const barraTxtElement = document.createElement("p")
-    const txtHtml = document.getElementById("txthtml");
+    const txtHtml = document.getElementById("html");
     barraTxtElement.innerHTML = barraTemplate;
     progressHtml.appendChild(barraTxtElement);
     progressHtml.style.setProperty("--progress", `${value}% `);
     barraTxtElement.style.fontSize= "25px";
     txtHtml.style.color=("yellow");
-
+    barraTxtElement.style.color= ("yellow");
+    barraTxtElement.style.marginTop = "35px";
     progressHtml.removeEventListener("click", unaFuctionHtml);
 }
 
+// Evento barra javascript
 progressJavaScript.addEventListener("click", unaFuctionJavascript)
-
 function unaFuctionJavascript (e){
     const value = 30 ;
     const barraTemplate = "30%";
     const barraTxtElement = document.createElement("p");
-    const txtJavascript = document.getElementById("javascript");
+    const txtJavascript = document.getElementById("javascript-2");
     barraTxtElement.innerHTML = barraTemplate;
     progressJavaScript.appendChild(barraTxtElement);
     progressJavaScript.style.setProperty("--progress", `${value}%`);
     barraTxtElement.style.fontSize= "25px";
     txtJavascript.style.color=("yellow");
-
+    barraTxtElement.style.color= ("yellow");
+    barraTxtElement.style.marginTop = "35px";
     progressJavaScript.removeEventListener("click", unaFuctionJavascript)
+}
+
+// Evento Python 
+
+progressPython.addEventListener("click", unaFuctionPython)
+function unaFuctionPython (e){
+    const value = 30 ;
+    const barraTemplate = "30%";
+    const barraTxtElement = document.createElement("p");
+    const txtPython = document.getElementById("python-2");
+    barraTxtElement.innerHTML = barraTemplate;
+    progressPython.appendChild(barraTxtElement);
+    progressPython.style.setProperty("--progress", `${value}%`);
+    barraTxtElement.style.fontSize= "25px";
+    txtPython.style.color=("yellow");
+    barraTxtElement.style.color= ("yellow");
+    barraTxtElement.style.marginTop = "35px";
+    progressPython.removeEventListener("click", unaFuctionPython)
+}
+
+//  Evento SQL 
 
 
+progressSql.addEventListener("click", unaFuctionSql)
+function unaFuctionSql (e){
+    const value = 20 ;
+    const barraTemplate = "20%";
+    const barraTxtElement = document.createElement("p");
+    const txtSql = document.getElementById("sql-2");
+    barraTxtElement.innerHTML = barraTemplate;
+    progressSql.appendChild(barraTxtElement);
+    progressSql.style.setProperty("--progress", `${value}%`);
+    barraTxtElement.style.fontSize= "25px";
+    barraTxtElement.style.color= ("yellow");
+    barraTxtElement.style.marginTop = "35px";
+    txtSql.style.color=("yellow");
+    progressSql.removeEventListener("click", unaFuctionSql)
+}
+
+//  Eevento  React 
+progressReact.addEventListener("click", unaFuctionReact)
+function unaFuctionReact (e){
+    const value = 30 ;
+    const barraTemplate = "30%";
+    const barraTxtElement = document.createElement("p");
+    const txtReact = document.getElementById("react-2");
+    barraTxtElement.innerHTML = barraTemplate;
+    progressReact.appendChild(barraTxtElement);
+    progressReact.style.setProperty("--progress", `${value}%`);
+    barraTxtElement.style.fontSize= "25px";
+    barraTxtElement.style.color= ("yellow");
+    barraTxtElement.style.marginTop = "35px";
+    txtReact.style.color=("yellow");
+    progressReact.removeEventListener("click", unaFuctionReact)
+}
+
+//  Evento github
+progressGithub.addEventListener("click", unaFuctionGithub)
+function unaFuctionGithub(e){
+    const value = 60 ;
+    const barraTemplate = "60%";
+    const barraTxtElement = document.createElement("p");
+    const txtGithub = document.getElementById("github-2");
+    barraTxtElement.innerHTML = barraTemplate;
+    progressGithub.appendChild(barraTxtElement);
+    progressGithub.style.setProperty("--progress", `${value}%`);
+    barraTxtElement.style.fontSize= "25px";
+    barraTxtElement.style.color= ("yellow");
+    barraTxtElement.style.marginTop = "35px";
+    txtGithub.style.color=("yellow");
+    progressGithub.removeEventListener("click", unaFuctionGithub)
 }
 
 
 
-
-
-progressPython.addEventListener("click",() =>{
-    const value = 10 ;
-    progressPython.style.setProperty("--progress", `${value}%`)
-})
-progressJava.addEventListener("click",() =>{
-    const value = 10;
-    progressJava.style.setProperty("--progress", `${value}%`)
-})
-progressReact.addEventListener("click",() =>{
-    const value = 20;
-    progressReact.style.setProperty("--progress", `${value}%`)
-})
-progressGithub.addEventListener("click",() =>{
-    const value = 50;
-    progressGithub.style.setProperty("--progress", `${value}%`)
-})
